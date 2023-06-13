5393e78e3e424cc9d5025f06a1e814aad1cce91c
Zheng Kai <zhengkai@gmail.com>
2023-06-13 17:44:37 +0800

    test

--- misc/test/completion.sh
+++ misc/test/completion.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -ex
 
 BASE="${OPENAI_API_BASE:-https://api.openai.com/v1}"
-
-curl "${BASE}/chat/completions" \
+curl "${BASE}/completions" \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer ${OPENAI_API_KEY}" \
   -d '{
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": "Hello!"}]
+    "model": "text-davinci-003",
+    "prompt": "Say this is a test",
+    "max_tokens": 7,
+    "temperature": 0
   }'
